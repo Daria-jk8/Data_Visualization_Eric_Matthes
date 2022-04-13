4402ea9ea5976b64d9df8683789b2ee055bc4afa
Daria-jk8 <92945302+Daria-jk8@users.noreply.github.com>
2022-04-13 16:35:07 +0300

    Update eq_world_map.py

--- eq_world_map.py
+++ eq_world_map.py
@@ -36,8 +36,8 @@ data = [{
         'colorbar':{'title': 'Magnitude'},
     },
 }]
-my_layout = Layout(title='Global Eartquakes')
+my_layout = Layout(title='Global Earthquakes')
 
 fig = {'data':data, 'layout':my_layout}
-offline.plot(fig, filename='global_eartquakes.html')
+offline.plot(fig, filename='global_earthquakes.html')
 
